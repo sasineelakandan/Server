ae6eee104de6482c2916c2fdff60801f5c85f014
sasineelakandan <devasasi887@gmail.com>
2024-12-27 18:57:42 +0530

    change

--- dist/models/Otp.validator.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.validateOtpCode = void 0;
-const validateOtpCode = (otp) => {
-    return /^\d{6}$/.test(otp);
-};
-exports.validateOtpCode = validateOtpCode;
